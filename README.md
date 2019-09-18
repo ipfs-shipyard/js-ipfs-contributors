d65a701f28a34f8c21e87cdd6455483bb46bd9d6
achingbrain <alex@achingbrain.net>
2019-09-18 13:22:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# js-ipfs-contributors
+
+> Script to generate a list of the contributors to js-IPFS since the last release
+
+![Screenshot](https://raw.githubusercontent.com/ipfs-shipyard/js-ipfs-contributors/master/screenshot.png)
+
+## Install
+
+Install dependences:
+
+* [Node.js](https://nodejs.org/en/)
+
+Then:
+
+```sh
+npm install -g js-ipfs-contributors
+```
+
+Or, install from source:
+
+```sh
+# Clone the repo
+git clone https://github.com/ipfs-shipyard/js-ipfs-contributors.git
+cd js-ipfs-contributors
+
+# Install project dependencies
+npm install
+```
+
+## Usage
+
+Simply run the tool by typing the following at the command line:
+
+```sh
+js-ipfs-contributors
+```
+
+Or, if you've installed from source:
+
+```sh
+npm start
+```
+
+Then follow the prompts. You will need a [Github personal access token](https://github.com/settings/tokens/) with the following scopes:
+* read_org
+* user_email
+
+Note: It may take a long time! Please be patient!
+
+## License
+
+The js-ipfs Project is dual-licensed under Apache 2.0 and MIT terms:
+
+- Apache License, Version 2.0, ([LICENSE-APACHE](https://github.com/ipfs/go-ipfs/blob/master/LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](https://github.com/ipfs/go-ipfs/blob/master/LICENSE-MIT) or http://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+const pkg = require('./package.json')
+const ora = require('ora')
+const log = require('debug')(pkg.name)
+const Chalk = require('chalk')
+const Inquirer = require('inquirer')
+const nyc = require('name-your-contributors/src/index')
+const getList = require('./contributions-list')
+const Config = require('./config')
+const fetch = require('node-fetch')
+
+async function main ({ argv, env }) {
+  console.log(`${Chalk.cyan('⬢')} ${Chalk.bold(Chalk.whiteBright('js-IPFS Contributors'))}`)
+  const spinner = ora()
+
+  try {
+    let githubToken
+
+    if (env.IPFS_CONTRIBUTORS_GITHUB_TOKEN) {
+      console.log(Chalk.white('Welcome to the js-IPFS contributors list generator!'))
+      githubToken = env.IPFS_CONTRIBUTORS_GITHUB_TOKEN
+    } else {
+      console.log(Chalk.white('Welcome to the js-IPFS contributors list generator! All you need is a Github token. Learn how to get one at https://github.com/mntnr/name-your-contributors#api-limits-and-setting-up-a-github-token'))
+
+      const { token } = await Inquirer.prompt([{
+        type: 'password',
+        name: 'token',
+        message: 'Enter your Github personal access token:',
+        validate: Boolean
+      }])
+
+      githubToken = token
+    }
+
+    const repos = Array.from(Config.repos)
+
+    while (true) {
+      console.log(Chalk.white('The following repos will be used:'))
+
+      repos.forEach((r, i) => {
+        console.log(Chalk.white((i + 1) + '. ') + Chalk.gray('github.com/') + r)
+      })
+
+      const { another, repo } = await Inquirer.prompt([{
+        type: 'confirm',
+        name: 'another',
+        message: 'Add another repo?',
+        default: false
+      }, {
+        type: 'input',
+        name: 'repo',
+        message: 'Enter repo (org/repo):',
+        when: ({ another }) => another
+      }])
+
+      if (!another) break
+      repos.push(repo)
+    }
+
+    const lastRelease = await fetch('https://api.github.com/repos/ipfs/js-ipfs/releases/latest')
+      .then(res => res.json())
+
+    const contributions = {}
+
+    for (const orgRepo of repos) {
+      spinner.start(`${Chalk.white('Getting contributors to')} ${Chalk.grey('github.com/') + orgRepo}`)
+
+      const [ user, repo ] = orgRepo.split('/')
+
+      contributions[orgRepo] = await nyc.repoContributors({
+        token: githubToken,
+        user,
+        repo,
+        before: new Date(),
+        after: new Date(lastRelease.published_at),
+        commits: true
+      })
+
+      spinner.stopAndPersist({
+        symbol: '❤️ ',
+        text: `${Chalk.white('Successfully got contributors for')} ${Chalk.grey('github.com/') + orgRepo}`
+      })
+    }
+
+    console.log(getList(contributions))
+    process.exit()
+  } catch (err) {
+    log(err)
+    spinner.fail(err.message || err.statusMessage)
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+main(process)
